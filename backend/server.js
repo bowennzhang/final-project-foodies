3c6ed0bf7d78bcbbbc3da63571780c632a5db1b3
bowennzhang <bowenofzhang@gmail.com>
2022-06-03 11:07:17 -0400

    update server

--- backend/server.js
+++ backend/server.js
@@ -16,12 +16,12 @@ express()
   .use(express.static("public"))
 
   // this is our catch all endpoint.
-  //   .get("*", (req, res) => {
-  //     res.status(404).json({
-  //       status: 404,
-  //       message: "This is obviously not what you are looking for.",
-  //     });
-  //   })
+  .get("*", (req, res) => {
+    res.status(404).json({
+      status: 404,
+      message: "This is obviously not what you are looking for.",
+    });
+  })
 
   // Node spins up our server and sets it to listen on port 8000.
   .listen(8000, () => console.log(`Listening on port 8000`));
